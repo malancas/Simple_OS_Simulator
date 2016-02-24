ced71e57824128c08bba079ba5db0e59fe1b37eb
Meredith Lancaster <malancaster0@gmail.com>
2016-02-24 16:46:04 -0500

    Added simple device input for testing other functionality, added device queue resizing functions

--- DeviceQueues.cpp
+++ DeviceQueues.cpp
@@ -1,16 +1,23 @@
 #include <unordered_map>
 #include <queue>
+#include <vector>
 using namespace std;
 
 struct DeviceQueues{
   //VARIABLES
   /* unordered hash that associates device name input with
      queue that was specified during sysgen */ 
-  
-
+  vector<queue<int>> printerQueues;
+  vector<queue<int>> diskQueues;
+  vector<queue<int>> cdQueues;
 
   //CONTRUCTORS
-  DeviceQueues()
+  DeviceQueues();
 
   //FUNCTIONS
+  makeQueues(int& pNum, int& dNum, int& cNum){
+    printerQueues.resize(pNum);
+    diskQueues.resize(dNum);
+    cdQueues.resize(cNum);
+  }
 };
--- main.cpp
+++ main.cpp
@@ -2,11 +2,32 @@
 #include <string>
 #include <long_opts>
 #include <unorderd_map>
+#include "DeviceQueues"
 using namespace std;
 
-int main(int argc, char* argv[]){
-  string input = "";
+void parseInput(const string& in, DeviceQueues& d){
+   int pNum = in[0];
+   int dNUm = in[4];
+   int cNum = in[8];
 
+   d.makeQueues(pNum, dNum, cNum);
+}
+
+int main(){
+  //sysgen
+  string input = "";
+  
+  //Include try/catch for input, error catching
+  //Ask for number of printer, disk, CD devices, respectively
+  cout << "Enter devices in format '<num>P, <num>D, <num>C' " << '\n';
+  cin >> input;
+
+  DeviceQueues d;
+
+  parseInput(input, d);
+
+  //running
+  
   /*
     Use hash for processes
     Must be accesible by ready queue, device queues, CPU
