5b4e0f8870350361eec5a1e06e8e851e08e78747
Meredith Lancaster <malancaster0@gmail.com>
2016-02-25 21:19:52 -0500

    Added additional interrupt/system call input functionality

--- CPU.cpp
+++ CPU.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iterator>
 using namespace std;
 
 struct CPU : public Memory {
@@ -16,6 +17,8 @@ struct CPU : public Memory {
     waitForInput();
   }
 
+  //A, t, p<num>, P<num>, d<num>, D<num>, c<num>, C<num>
+  //system calls: S -> s, d, c, r
   void waitForInput(){
     string input = "";
     while (input != "t"){
@@ -24,17 +27,50 @@ struct CPU : public Memory {
         cout << "No processes running. Add processes and try again" << '\n';
       }
       else if (emptyCPU && input == "A"){
-        processes.insert(make_pair(pidCounter,Process(pidCounter, pcbCounter));
-        currProcess = pidCounter;
+        processes.insert(make_pair(pidCounter,Process(pidCounter, pcbCounter)));
+        if (readyQueue.empty()){
+          currProcess = pidCounter;
+          emptyCPU = false;          
+        }
+        else {
+          currProcess = readyQueue.front();
+          readyQueue.pop();
+          readyQueue.push(pidCounter);
+        }
         ++pidCounter; ++pcbCounter;
-        emptyCPU = false;
       }
       else if (!emptyCPU && input == "A"){
-        processes.insert(make_pair(pidCounter,Process(pidCounter, pcbCounter));
+        processes.insert(make_pair(pidCounter,Process(pidCounter, pcbCounter)));
         readyQueue.push(pidCounter);
         ++pidCounter; ++pcbCounter;
       }
+      else if (input == "t"){
+        if (emptyCPU){
+          cout << "Error: CPU is unoccupied, no process can be terminated" << '\n';
+        }
+        else {
+          processes.erase(currProcess);
+          currProcess = readyQueue.front();
+          readyQueue.pop();
+        }
+      }
+      else if (input == "S"){
+        cout << "Enter one of the following: r,p,c,d" << '\n';
+        cin >> input;
+        if (input == "r"){
+          cout << input;
+        }
+        else if (input == "p"){
+          cout << input;
+        }
+        else if (input == "c"){
+          cout << input;
+        }
+        //input == d
+        else {
+          cout << input;
+        } 
+      }
     }
   }
-
 };
