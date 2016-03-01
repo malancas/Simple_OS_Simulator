35bd13aa82f6b03351f6f19aa99bea69da9aac93
Meredith Lancaster <malancaster0@gmail.com>
2016-03-01 10:51:48 -0500

    Updated sys gen input error checking with non numerical character checking

--- Sysgen.cpp
+++ Sysgen.cpp
@@ -1,41 +1,59 @@
 #include <string>
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include "Memory.cpp"
 using namespace std;
 
 struct Sysgen : public Memory {
-	class NegativeError {};
+	//VARIABLES
+  class NegativeError {};
+  bool goodInput;
+  int num;
 
-  	//FUNCTIONS
-  	void getUserDeviceInput(){
-  		int num = 0;
-  		checkInputForErrors("Enter the number of printer devices: ", num);
-  		printerQueues.resize(num);
+  //CONSTRUCTORS
+  Sysgen() : goodInput(false), num(0) {}
 
-  		checkInputForErrors("Enter the number of disk devices: ", num);
-  		diskQueues.resize(num);
+  //FUNCTIONS
+  void getUserDeviceInput(){
+  	checkInputForErrors("Enter the number of printer devices: ");
+  	printerQueues.resize(num);
+    goodInput = false;
+
+  	checkInputForErrors("Enter the number of disk devices: ");
+  	diskQueues.resize(num);
+    goodInput = false;
   		
-  		checkInputForErrors("Enter the number of CD devices: ", num);
-  		cdQueues.resize(num);
-  	}
+  	checkInputForErrors("Enter the number of CD devices: ");
+  	cdQueues.resize(num);
+  }
 
-  	void checkInputForErrors(const string& userMessage, int& num){
-  		bool goodInput = false;
-  		while (!goodInput) {
-        goodInput = true;
-    		try {
-      			cout << userMessage << '\n';
-      			cin >> num;
-
-      			if (num < 0){
-        			throw NegativeError();
-      			}
-    		}
-    
-    		catch (NegativeError){
-      			cerr << "Negative number entered. Try again." << '\n' << '\n';
-      			goodInput = false;      
-    		}
-  		}
-  	}
+  void checkInputForErrors(const string& userMessage){
+    while (!goodInput){
+      cout << userMessage << '\n';
+      string line;
+      if (getline(cin, line)) {
+        istringstream iss{line};
+        //Checks if the input can be converted to an int
+        if (iss >> num && (iss.eof() || isspace(iss.peek()))) {
+          
+          //If it was successfully converted, then checks if
+          //num is negative
+          if (num < 0){
+            cerr << "Negative number entered. Please try again" << '\n';
+          }
+          
+          //If num is not negative, goodInput is set to true and loop breaks
+          //meaning that num can be used to set how many devices there will
+          //be of a certain type
+          else{
+            goodInput = true;            
+          }
+        }
+        else {
+          cerr << "Non numeric character entered. Please try again." << '\n';
+        }  
+      }   
+    }
+  }
 };
