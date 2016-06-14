f211aa112504b8e5637f37f682c403ca61eeb385
malancas <malancaster0@gmail.com>
2016-06-14 17:03:05 +0000

    Rearranged Memory.h for better readability and consistency

--- Memory.h
+++ Memory.h
@@ -13,7 +13,33 @@
 using namespace std;
 
 struct Memory {
+  //CONTRUCTORS
+  Memory();
+
+  //FUNCTORS
+  //The functors below are used for certain sorted data structures that follow
+  struct SortByLowTrack{
+    bool operator() (const int& x, const int& y) { return processes[x].track < processes[y].track; }
+  };
+
+  struct SortByHighTrack{
+    bool operator() (const int& x, const int& y) { return processes[x].track > processes[y].track; }
+  };
+
+  struct SortByLowBurst{
+    bool operator() (const int& x, const int& y) { return processes[x].remainingBurst < processes[y].remainingBurst; }
+  };
+
+  struct SortBySize{
+    bool operator() (const int& x, const int& y) { return processes[x].size > processes[y].size; }
+  };
+
   //VARIABLES
+  
+  //Holds boolean values, each corresponding
+  //to the disk vector that is acting as the
+  //scan vector.
+  vector<bool> scanDiskDequesStatus;
 
   /*
     Contains the Process objects, using their
@@ -63,16 +89,8 @@ struct Memory {
   int pageSize;
 
   
-//Represents cylinders in the hard drive
-vector<int> cylinders;
-
-  //Holds boolean values, each corresponding
-  //to the disk vector that is acting as the
-  //scan vector.
-  vector<bool> scanDiskDequesStatus;
-
-  //CONTRUCTORS
-  Memory();
+  //Represents cylinders in the hard drive
+  vector<int> cylinders;
 
   /*
     The ready deque contains process PIDS
@@ -80,47 +98,37 @@ vector<int> cylinders;
     device deques specified during the sys gen
     phase. Each of these deques contains PIDS
   */
-   deque<int> readyDeque;
-   vector<deque<int>> printerDeques;
-   vector<deque<int>> cdDeques;
-   vector<deque<int>> diskDeques0;
-   vector<deque<int>> diskDeques1;
-   vector<vector<int>> frameTable;
-   deque<int> freeFrameList;
+  deque<int> readyDeque;
 
-  //void addProcessToWaitingDeque(const int& pid, const int& dequeNum, const bool& zeroIsWaiting);
+  vector<deque<int>> printerDeques;
+  
+  vector<deque<int>> cdDeques;
+  
+  vector<deque<int>> diskDeques0;
+  
+  vector<deque<int>> diskDeques1;
+  
+  vector<vector<int>> frameTable;
+  
+  deque<int> freeFrameList;
+  
   void checkForSystemCallinDiskDeque(const int& callNum, const bool& zeroIsWaiting);
-  //void checkForSystemCallinDiskSet(const int& callNum, const bool& zeroIsWaiting);
+  
   void checkForSystemCallinDeque(vector<deque<int>>& devDeques, const int& callNum);
-   bool sortByLowestTrackFirst(const int& lhs, const int& rhs);
-   bool sortByHighestTrackFirst(const int& lhs, const int& rhs);
-   bool sortByLargestSizeFirst(const int& lhs, const int& rhs);
-
-  struct SortByLowTrack{
-		bool operator() (const int& x, const int& y) { return processes[x].track < processes[y].track; }
-  };
-
-  struct SortByHighTrack{
-		bool operator() (const int& x, const int& y) { return processes[x].track > processes[y].track; }
-  };
-
-  struct SortByLowBurst{
-  	bool operator() (const int& x, const int& y) { return processes[x].remainingBurst < processes[y].remainingBurst; }
-  };
-
-  struct SortBySize{
-  	bool operator() (const int& x, const int& y) { return processes[x].size > processes[y].size; }
-  };
-
-/*
-  auto f = [](const int& x, const int& y)
-  {return processes[x].size < processes[y].size;};
-*/
-
-   vector<multiset<int,SortByLowTrack>> diskSets0;
-   vector<multiset<int,SortByHighTrack>> diskSets1;
-   set<int,SortByLowBurst> readySet;
-   set<int,SortBySize> jobPool;
+  
+  bool sortByLowestTrackFirst(const int& lhs, const int& rhs);
+  
+  bool sortByHighestTrackFirst(const int& lhs, const int& rhs);
+  
+  bool sortByLargestSizeFirst(const int& lhs, const int& rhs);
+  
+  vector<multiset<int,SortByLowTrack>> diskSets0;
+  
+  vector<multiset<int,SortByHighTrack>> diskSets1;
+  
+  set<int,SortByLowBurst> readySet;
+  
+  set<int,SortBySize> jobPool;
 };
 
 #endif
